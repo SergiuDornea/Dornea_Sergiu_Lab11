0bc54524090c5e964f1ae483917fd474265a06c6
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-12-22 09:19:18 +0200

    up to 18

---
+++ Dornea_Sergiu_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dornea_Sergiu_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dornea_Sergiu_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using Dornea_Sergiu_Lab11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dornea_Sergiu_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
