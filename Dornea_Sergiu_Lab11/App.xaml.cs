36cfc3786e917f944b18a45bf0fa20440c6f86a3
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-12-22 09:31:05 +0200

    up to 26

--- Dornea_Sergiu_Lab11/App.xaml.cs
+++ Dornea_Sergiu_Lab11/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿namespace Dornea_Sergiu_Lab11
+﻿using Dornea_Sergiu_Lab11.Data;
+using Dornea_Sergiu_Lab11.Models;
+
+namespace Dornea_Sergiu_Lab11
+
 {
     public partial class App : Application
     {
+        public static ShoppingListDatabase Database { get; private set; }
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
         }
     }
 }
